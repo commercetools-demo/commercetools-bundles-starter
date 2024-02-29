b1b11ca44589d1cb4ffb9555cc4b85e7f50e0fee
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-02-29 09:41:44 +0100

    use ootb layout

--- packages/bundles-dynamic/src/components/create-bundle-form/create-bundle-form.js
+++ packages/bundles-dynamic/src/components/create-bundle-form/create-bundle-form.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { FormattedMessage, useIntl } from 'react-intl';
-import { useMutation } from '@apollo/client';
+import { useIntl } from 'react-intl';
 import { DOMAINS, GRAPHQL_TARGETS } from '@commercetools-frontend/constants';
-import Spacings from '@commercetools-uikit/spacings';
-import {
-  BackToList,
-  TabContainer,
-  View,
-  ViewHeader,
-} from '@commercetools-us-ps/bundles-core';
 import { ATTRIBUTES, BUNDLE_PRODUCT_TYPE, ROOT_PATH } from '../../constants';
 import { BundleForm } from '../bundle-form';
 import CreateBundle from './create-bundle.graphql';
 import messages from './messages';
 import { useShowNotification } from '@commercetools-frontend/actions-global';
-import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
+import {
+  useApplicationContext,
+  useMcMutation,
+} from '@commercetools-frontend/application-shell-connectors';
+import { InfoDetailPage } from '@commercetools-frontend/application-components';
 
-const CreateBundleForm = ({ match }) => {
+const CreateBundleForm = () => {
   const intl = useIntl();
   const { projectKey } = useApplicationContext((context) => ({
     projectKey: context.project.key ?? '',
   }));
   const mainRoute = `/${projectKey}/${ROOT_PATH}`;
   const showNotification = useShowNotification();
-  const [createBundle, { data, loading }] = useMutation(CreateBundle, {
+  const [createBundle, { data, loading }] = useMcMutation(CreateBundle, {
     onCompleted: () =>
       showNotification({
         kind: 'success',
@@ -80,36 +75,20 @@ const CreateBundleForm = ({ match }) => {
   }
 
   return (
-    <View>
-      <ViewHeader
-        title={<FormattedMessage {...messages.title} />}
-        backToList={
-          <BackToList
-            href={mainRoute}
-            label={intl.formatMessage(messages.backButton)}
-          />
-        }
+    <InfoDetailPage
+      onPreviousPathClick={() => history.push(mainRoute)}
+      title={intl.formatMessage(messages.title)}
+      previousPathLabel={intl.formatMessage(messages.backButton)}
+    >
+      <BundleForm
+        onSubmit={onSubmit}
+        data={data}
+        loading={loading}
+        redirect={mainRoute}
       />
-      <TabContainer>
-        <Spacings.Stack scale="m">
-          <BundleForm
-            onSubmit={onSubmit}
-            data={data}
-            loading={loading}
-            redirect={mainRoute}
-          />
-        </Spacings.Stack>
-      </TabContainer>
-    </View>
+    </InfoDetailPage>
   );
 };
 CreateBundleForm.displayName = 'CreateBundleForm';
-CreateBundleForm.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      projectKey: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
 
 export default CreateBundleForm;
