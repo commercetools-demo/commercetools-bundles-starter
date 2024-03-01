9fd9b6827a95a54a9f7661f19cc3e703bb2a740e
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-03-01 08:48:57 +0100

    build properly

--- packages/bundles-core/rollup.config.mjs
+++ packages/bundles-core/rollup.config.mjs
@@ -1,30 +1,30 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import graphql from '@rollup/plugin-graphql';
-import json from '@rollup/plugin-json';
-import postcss from 'rollup-plugin-postcss';
-import svgr from '@svgr/rollup';
+import graphql from "@rollup/plugin-graphql";
+import json from "@rollup/plugin-json";
+import postcss from "rollup-plugin-postcss";
+import svgr from "@svgr/rollup";
 import dts from "rollup-plugin-dts";
 
-import pkg from './package.json' assert { type: 'json' };
+import pkg from "./package.json" assert { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
     output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-        sourcemap: true,
-      },
+      // {
+      //   file: pkg.main,
+      //   format: "cjs",
+      //   sourcemap: true,
+      // },
       {
         file: pkg.module,
         format: "esm",
-        sourcemap: true,
-      },
+        sourcemap: true
+      }
     ],
-    external:[/node_modules/],
+    external: [...Object.keys(pkg.devDependencies), /node_modules/],
     plugins: [
       resolve(),
       commonjs(),
@@ -32,14 +32,17 @@ export default [
       graphql(),
       json(),
       svgr(),
-      postcss({extract: false,
-        modules: true,})
-    ],
+      postcss(
+        {
+          extract: false,
+          modules: true
+        })
+    ]
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.(css|graphql)$/],
-  },
+    external: [/\.(css|graphql)$/]
+  }
 ];
